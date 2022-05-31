880a53098f70a854671b6e20949b269eb5e063df
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-05-31 07:52:31 +0200

    feat: assignement 1 about variables

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,6 @@
+let country = 'France';
+let continent = 'Europe';
+let population = 67000000;
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -1,1 +1,4 @@
-console.log('Hello world');
+// console.log('Hello world');
+// const years3 = 'test';
+// console.log(years3);
+// console.log(typeof null);
