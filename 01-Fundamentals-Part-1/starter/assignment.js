dfc0245aad87b79cbb9fd998e0a0855bac4883b4
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-10-02 19:30:21 +0200

    feat(training): exercice with scores of animals

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -45,3 +45,87 @@ if (isMarkBMIhigherThanJohn)
 else console.log(`John's BMI ${BMIOfJohon} is higher than Mark ${BMIOfMark}`);
 // test
 // test
+
+// function helpers
+const calculateAverage = (array) =>
+    array.length && array.reduce((acc, score) => acc + score, 0) / array.length;
+
+const checkTheWinnerWithNoLimit = (
+    dolphinsAverageScore,
+    koalasAveragescore
+) => {
+    if (dolphinsAverageScore > koalasAveragescore)
+        console.log(
+            `Dolphins have won with the avrage of ${dolphinsAverageScore} over ${koalasAveragescore} 🏆`
+        );
+    else if (koalasAveragescore > dolphinsAverageScore)
+        console.log(
+            `Koalas have won with the average of ${koalasAveragescore} over ${dolphinsAverageScore}`
+        );
+    else
+        console.log(
+            `It is a draw with the scor of Doplohins equal ${dolphinsAverageScore} and Koals: ${koalasAveragescore}`
+        );
+};
+
+const checkTheWinnerWithLimit100 = (
+    dolphinsAverageScore,
+    koalasAveragescore
+) => {
+    if (
+        dolphinsAverageScore > koalasAveragescore &&
+        dolphinsAverageScore >= 100
+    )
+        console.log(
+            `Dolphins have won with the avrage of ${dolphinsAverageScore} over ${koalasAveragescore}`
+        );
+    else if (
+        koalasAveragescore > dolphinsAverageScore &&
+        koalasAveragescore >= 100
+    )
+        console.log(
+            `Koalas have won with the average of ${koalasAveragescore} over ${dolphinsAverageScore}`
+        );
+    else if (
+        dolphinsAverageScore === koalasAveragescore &&
+        dolphinsAverageScore >= 100 &&
+        koalasAveragescore >= 100
+    )
+        console.log(
+            `It is a draw with the scor of Doplohins equal ${dolphinsAverageScore} and Koals: ${koalasAveragescore}`
+        );
+    else console.log('No output for this gamme');
+};
+
+// CODING CHALLENG #3
+let dolphinsScores = [96, 108, 89];
+let averageDolphinsScore = calculateAverage(dolphinsScores);
+console.log(averageDolphinsScore.toFixed(2));
+
+let koalasScores = [88, 91, 110];
+let averageKoalasScore = calculateAverage(koalasScores);
+console.log(averageKoalasScore.toFixed(2));
+
+checkTheWinnerWithNoLimit(averageDolphinsScore, averageKoalasScore);
+
+// Bonus 1
+dolphinsScores = [97, 112, 101];
+averageDolphinsScore = calculateAverage(dolphinsScores);
+console.log(averageDolphinsScore.toFixed(2));
+
+koalasScores = [109, 95, 123];
+averageKoalasScore = calculateAverage(koalasScores);
+console.log(averageKoalasScore.toFixed(2));
+
+checkTheWinnerWithLimit100(averageDolphinsScore, averageKoalasScore);
+
+// Bonus 2
+dolphinsScores = [97, 112, 101];
+averageDolphinsScore = calculateAverage(dolphinsScores);
+console.log(averageDolphinsScore.toFixed(2));
+
+koalasScores = [109, 95, 106];
+averageKoalasScore = calculateAverage(koalasScores);
+console.log(averageKoalasScore.toFixed(2));
+
+checkTheWinnerWithLimit100(averageDolphinsScore, averageKoalasScore);
