d8f5af4262cef1a700ab161447f1cca25d9208e5
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-10-04 00:43:53 +0200

    feat(training): exercices on objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -36,31 +36,69 @@
 // console.log(total.map((val, index) => val + tips[index]));
 
 //Exercice on object
-const jonas = {
-    firstname: 'Jonas',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    calcAge: function () {
-        return (this.age = 2037 - this.birthYear);
-    },
-    hasAdriverLicence: true,
-    introduceJonas: function () {
-        return `${this.firstname} is a ${this.calcAge()}-year old ${
-            this.job
-        }, and he ${
-            this.hasAdriverLicence ? 'has' : ' has not'
-        } a driver's licence and another age which is ${this.age2}`;
+// const jonas = {
+//     firstname: 'Jonas',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     calcAge: function () {
+//         return (this.age = 2037 - this.birthYear);
+//     },
+//     hasAdriverLicence: true,
+//     introduceJonas: function () {
+//         return `${this.firstname} is a ${this.calcAge()}-year old ${
+//             this.job
+//         }, and he ${
+//             this.hasAdriverLicence ? 'has' : ' has not'
+//         } a driver's licence and another age which is ${this.age2}`;
+//     },
+// };
+
+// console.log(
+//     `${jonas.firstname} has ${jonas['friends'].length} and his best friend is ${jonas.friends[0]}`
+// );
+
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+
+// console.log(jonas.introduceJonas());
+// jonas.age2 = jonas.calcAge();
+// console.log(jonas.introduceJonas());
+
+//CODING CHALLENGE #3
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        return (this.BMI = this.mass / this.height ** 2);
     },
 };
 
-console.log(
-    `${jonas.firstname} has ${jonas['friends'].length} and his best friend is ${jonas.friends[0]}`
-);
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        return (this.BMI = this.mass / (this.height * this.height));
+    },
+};
 
-console.log(jonas.calcAge());
-console.log(jonas.age);
+john.calcBMI();
+mark.calcBMI();
 
-console.log(jonas.introduceJonas());
-jonas.age2 = jonas.calcAge();
-console.log(jonas.introduceJonas());
+if (mark.BMI > john.BMI) {
+    console.log(
+        `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+            john.fullName
+        }'s (${john.BMI})`
+    );
+} else if (john.BMI > mark.BMI) {
+    console.log(
+        `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s (${mark.BMI})`
+    );
+} else {
+    console.log(
+        `${john.fullName}'s BMI (${john.BMI}) is same as ${mark.fullName}'s (${mark.BMI})`
+    );
+}
