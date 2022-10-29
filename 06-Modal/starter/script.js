2889df1a6901c4418080a1adbae1aac8d2ec256c
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-10-29 16:50:23 +0200

    feat(training): display or hide modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const btns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtnModal = document.querySelector('.close-modal');
+
+const hiddenModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+overlay.addEventListener('click', hiddenModal);
+closeBtnModal.addEventListener('click', hiddenModal);
