59010a9d2ddb01d121a9f14a46c5bf2218a03811
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-11-28 00:28:47 +0900

    feat(training): challenge 4 on strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -267,6 +263,7 @@ delayed_departure
 */
 
 // CHALLENGE 4 after gettting the correction
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', () => {
@@ -277,4 +274,22 @@ document.querySelector('button').addEventListener('click', () => {
     let word = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
     console.log(`${word.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
+});
+*/
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flightsRow = flights.split('+');
+flightsRow.forEach(flightRow => {
+  const [status, from, to, time] = flightRow.split(';');
+  console.log(
+    `${status.startsWith('_Delayed') ? 'D' : ' '} ${status.replaceAll(
+      '_',
+      ' '
+    )} from ${from.slice(0, 3).toUpperCase()} to ${to
+      .slice(0, 3)
+      .toUpperCase()} ${time.replace(':', 'h')}`.padStart(45, ' ')
+  );
 });
