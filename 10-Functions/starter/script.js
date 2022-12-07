bd9544c41aa5827291f229320cb813b9472c5910
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-12-07 11:51:54 +0900

    feat(training): exercice on function with bind

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,3 +1,16 @@
 'use strict';
+
+//Exercice on functions return another functions
 const greet = greeting => name => console.log(`${greeting} ${name}`);
-greet('hey')('Thomas');
+//greet('hey')('Thomas');
+
+const addVAT = value => () => value + value * 0.23;
+//console.log(addVAT(100)());
+
+const addVAT2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+console.log(addVAT2.bind(null, 0.23)().bind(null, 100)());
