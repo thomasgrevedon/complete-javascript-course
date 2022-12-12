182b63ba8885c4dc43083cc43b65c8ce429b4061
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-12-12 12:51:40 +0900

    feat(training): challenge on prototype functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+// Challenge on prototype
+
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(
+    'Speed of the car ',
+    this.make,
+    'after acceleration ',
+    this.speed
+  );
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log('Speed of the car after brake ', this.speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+car1.accelerate();
+car1.brake();
+car2.accelerate();
+car2.brake();
