d9c0046eff4552fdcf1de9dcd074cbe35cd934ec
Thomas Grevedon <thomas.grevedon@gmail.com>
2022-12-13 22:09:47 +0900

    feat(bankist): display banking movements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -73,6 +73,24 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const addMovement = movements => {
+  containerMovements.innerHTML = '';
+
+  movements.forEach((movement, i) => {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${movement} €</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+addMovement(movements);
+
 /////////////////////////////////////////////////
 
 // EXERCICES AND CHALLENGE
