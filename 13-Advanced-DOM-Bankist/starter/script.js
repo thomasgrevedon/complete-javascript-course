f55bca1d21ecc534918fe5a1e1020329662457d4
Thomas Grevedon <thomas.grevedon@gmail.com>
2023-02-05 20:18:28 +0900

    feat(TRAINING): and mouseover event on button

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const navBarLinks = document.querySelector('.nav__links');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -41,16 +42,49 @@ document
   .querySelector('.button-close-cookie')
   .addEventListener('click', () => message.remove());
 
-const learnMore = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const learnMore = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-learnMore.addEventListener('click', () => {
-  //const section1Pos = section1.getBoundingClientRect();
-  //console.log(e.target.getBoundingClientRect());
-  // window.scrollTo({
-  //   left: section1Pos.left + window.scrollX,
-  //   top: section1Pos.top + window.scrollY,
-  //   behavior: 'smooth',
-  // });
-  section1.scrollIntoView({ behavior: 'smooth' });
+// learnMore.addEventListener('click', () => {
+//   //const section1Pos = section1.getBoundingClientRect();
+//   //console.log(e.target.getBoundingClientRect());
+//   // window.scrollTo({
+//   //   left: section1Pos.left + window.scrollX,
+//   //   top: section1Pos.top + window.scrollY,
+//   //   behavior: 'smooth',
+//   // });
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+navBarLinks.addEventListener('click', e => {
+  e.preventDefault();
+  const el = e.target;
+  const section = el.getAttribute('href');
+  if (section)
+    document.querySelector(section).scrollIntoView({ behavior: 'smooth' });
+});
+
+const tabContainer = document.querySelector('.operations__tab-container');
+const operationsTabs = document.querySelectorAll('.operations__tab');
+const opContents = document.querySelectorAll('.operations__content');
+
+// event listener on buttons et find closest parent
+// not button then return
+// remove all active on button and content on text area
+// add active class on clicked button and text area
+
+tabContainer.addEventListener('mouseover', e => {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  operationsTabs.forEach(opTab =>
+    opTab.classList.remove('operations__tab--active')
+  );
+  opContents.forEach(opContent =>
+    opContent.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
