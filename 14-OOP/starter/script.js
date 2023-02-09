e05fd319a6ed9cc21c56e383577cf05404859552
Thomas Grevedon <thomas.grevedon@gmail.com>
2023-02-09 20:37:10 +0900

    feat(training): OOP challenges

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,34 +1,35 @@
 'use strict';
 // Challenge on prototype
 
-// const Car = function (make, speed) {
-//   this.make = make;
-//   this.speed = speed;
-// };
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
 
-// Car.prototype.accelerate = function () {
-//   this.speed += 10;
-//   console.log(
-//     'Speed of the car ',
-//     this.make,
-//     'after acceleration ',
-//     this.speed
-//   );
-// };
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(
+    'Speed of the car ',
+    this.make,
+    'after acceleration ',
+    this.speed
+  );
+};
 
-// Car.prototype.brake = function () {
-//   this.speed -= 5;
-//   console.log('Speed of the car after brake ', this.speed);
-// };
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log('Speed of the car after brake ', this.speed);
+  return this;
+};
 
-// const car1 = new Car('BMW', 120);
-// const car2 = new Car('Mercedes', 95);
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
 // car1.accelerate();
 // car1.brake();
 // car2.accelerate();
 // car2.brake();
 
-// Coding Challenge #1
+//Coding Challenge #1
 
 /* 
 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
@@ -51,39 +52,113 @@ DATA CAR 1: 'Ford' going at 120 km/h
 
 GOOD LUCK 😀
 */
-class Car {
-  constructor(make, speed){
-    this.make = make;
-    this.speed = speed;
+// class Car {
+//   constructor(make, speed){
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate(){
+//     this.speed += 10;
+//     console.log(
+//             'Speed of the car ',
+//             this.make,
+//             'after acceleration ',
+//             this.speed
+//           );
+//   }
+//   brake(){
+//     this.speed -= 5;
+//     console.log('Speed of the car after brake ', this.speed);
+//   }
+//   get speed(){
+//     console.log("going in getter");
+//     return this._speed / 1.6;
+//   }
+//   set speed(val){
+//     console.log("going in setter");
+//     this._speed = val * 1.6;
+//   }
+// }
+// const myCar = new Car("Ford", 10);
+// myCar.accelerate();
+// myCar.brake();
+// myCar.speed;
+// myCar.speed = 100;
+// myCar.speed;
+// myCar.accelerate();
+// console.log(myCar);
+
+//CHALLENGE 3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge += 1;
+//   console.log(
+//     `${this.make} going at ${this.speed}, with a charge of ${this.charge}`
+//   );
+// };
+
+// const teslaCar = new EV('Tesla', 120, 23);
+// teslaCar.accelerate();
+// teslaCar.brake();
+// teslaCar.chargeBattery(30);
+// teslaCar.accelerate();
+
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class EV extends Car {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
   }
 
-  accelerate(){
-    this.speed += 10;
+  accelerate() {
+    this.speed += 20;
+    this.#charge += 1;
     console.log(
-            'Speed of the car ',
-            this.make,
-            'after acceleration ',
-            this.speed
-          );
-  }
-  brake(){
-    this.speed -= 5;
-    console.log('Speed of the car after brake ', this.speed);
-  }
-  get speed(){
-    console.log("going in getter");
-    return this._speed / 1.6;
-  }
-  set speed(val){
-    console.log("going in setter");
-    this._speed = val * 1.6;
+      `${this.make} going at ${this.speed}, with a charge of ${this.#charge}`
+    );
+    return this;
   }
 }
-const myCar = new Car("Ford", 10);
-myCar.accelerate();
-myCar.brake();
-myCar.speed;
-myCar.speed = 100;
-myCar.speed;
-myCar.accelerate();
-console.log(myCar);
+
+const newCar = new EV('Rivian', 120, 23);
+newCar.chargeBattery(30).accelerate().brake().accelerate();
